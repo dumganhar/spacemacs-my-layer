cd62ed2645fc86da6b7483e52235719e0c5c2f29
andyque <guanghui8827@126.com>
2015-04-29 10:21:53 +0800

    fix config error

--- my-misc/config.el
+++ my-misc/config.el
@@ -1,119 +1,38 @@
 ;;add abbrev mode
-(require 'use-package)
+(define-abbrev-table 'global-abbrev-table '(
 
-(use-package abbrev
-  :defer t
-  :config
-  (progn
+                                            ;; math/unicode symbols
+                                            ("8in" "∈")
+                                            ("8nin" "∉")
+                                            ("8inf" "∞")
+                                            ("8luv" "♥")
+                                            ("8smly" "☺")
 
-    (define-abbrev-table 'global-abbrev-table '(
+                                            ;; email
+                                            ("8me" "guanghui.qu@cocos2d-x.org")
 
-                                                ;; math/unicode symbols
-                                                ("8in" "∈")
-                                                ("8nin" "∉")
-                                                ("8inf" "∞")
-                                                ("8luv" "♥")
-                                                ("8smly" "☺")
+                                            ;; computing tech
+                                            ("8wp" "Wikipedia")
+                                            ("8ms" "Microsoft")
+                                            ("8g" "Google")
+                                            ("8it" "IntelliType")
+                                            ("8msw" "Microsoft Windows")
+                                            ("8win" "Windows")
+                                            ("8ie" "Internet Explorer")
+                                            ("8ahk" "AutoHotkey")
+                                            ("82dx" "Cocos2D-X")
 
-                                                ;; email
-                                                ("8me" "guanghui.qu@cocos2d-x.org")
+                                            ;; signature
+                                            ("zl" "zilongshanren")
+                                            ;; emacs regex
+                                            ("8d" "\\([0-9]+?\\)")
+                                            ("8str" "\\([^\"]+?\\)\"")))
 
-                                                ;; computing tech
-                                                ("8wp" "Wikipedia")
-                                                ("8ms" "Microsoft")
-                                                ("8g" "Google")
-                                                ("8it" "IntelliType")
-                                                ("8msw" "Microsoft Windows")
-                                                ("8win" "Windows")
-                                                ("8ie" "Internet Explorer")
-                                                ("8ahk" "AutoHotkey")
-                                                ("82dx" "Cocos2D-X")
-
-                                                ;; signature
-                                                ("zl" "zilongshanren")
-                                                ;; emacs regex
-                                                ("8d" "\\([0-9]+?\\)")
-                                                ("8str" "\\([^\"]+?\\)\"")
-
-
-                                                ))
-
-    ;; stop asking whether to save newly added abbrev when quitting emacs
-    (setq save-abbrevs nil)
+;; stop asking whether to save newly added abbrev when quitting emacs
+(setq save-abbrevs nil)
 
     ;; turn on abbrev mode globally
-    (setq-default abbrev-mode t)
-    )
-  :diminish abbrev-mode
-  )
+(setq-default abbrev-mode t)
 
-;; Githu PR settings
-;;http://endlessparentheses.com/easily-create-github-prs-from-magit.html
-(defun endless/visit-pull-request-url ()
-  "Visit the current branch's PR on Github."
-  (interactive)
-  (browse-url
-   (format "https://github.com/%s/compare/%s"
-           (replace-regexp-in-string
-            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
-            (magit-get "remote"
-                       (magit-get-current-remote)
-                       "url"))
-           (magit-get-current-branch))))
 
-(eval-after-load 'magit
-  '(define-key magit-mode-map "V"
-     #'endless/visit-pull-request-url))
 
-(defadvice magit-blame-mode (after magit-blame-change-to-emacs-state activate compile)
-  "when entering magit blame mode, change evil normal state to emacs state"
-  (if (evil-normal-state-p)
-      (evil-emacs-state)
-    (evil-normal-state))
-  )
-
-(ad-activate 'magit-blame-mode)
-
-(defadvice git-timemachine-mode (after git-timemachine-change-to-emacs-state activate compile)
-  "when entering git-timemachine mode, change evil normal state to emacs state"
-  (if (evil-normal-state-p)
-      (evil-emacs-state)
-    (evil-normal-state)))
-
-(ad-activate 'git-timemachine-mode)
-
-(defun my-vc-visit-file-revision (file rev)
-  "Visit revision REV of FILE in another window.
-With prefix argument, uses the current window instead.
-If the current file is named `F', the revision is named `F.~REV~'.
-If `F.~REV~' already exists, use it instead of checking it out again."
-  ;; based on `vc-revision-other-window'.
-  (interactive
-   (let ((file (expand-file-name
-                (read-file-name
-                 (if (buffer-file-name)
-                     (format "File (%s): " (file-name-nondirectory
-                                            (buffer-file-name)))
-                   "File: ")))))
-     (require 'vc)
-     (unless (vc-backend file)
-       (error "File %s is not under version control" file))
-     (list file (vc-read-revision
-                 "Revision to visit (default is working revision): "
-                 (list file)))))
-  (require 'vc)
-  (unless (vc-backend file)
-    (error "File %s is not under version control" file))
-  (let ((revision (if (string-equal rev "")
-                      (vc-working-revision file)
-                    rev))
-        (visit (if current-prefix-arg
-                   'switch-to-buffer
-                 'switch-to-buffer-other-window)))
-    (funcall visit (vc-find-revision file revision))))
-
-(require 'git-messenger)
-(define-key git-messenger-map (kbd "f") 'my-vc-visit-file-revision)
-
-(require 'magit)
-(define-key magit-log-mode-map (kbd "W") 'magit-copy-item-as-kill)
--- my-misc/packages.el
+++ my-misc/packages.el
@@ -15,6 +15,7 @@
     ;; package my-miscs go here
     swiper
     magit
+    git-messenger
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
@@ -64,4 +65,82 @@ which require an initialization must be listed explicitly in the list.")
 
 
 (defun my-misc/post-init-magit ()
+  (use-package magit 
+    :defer t
+    :config
+    (progn
+     ;; Githu PR settings
+     ;;http://endlessparentheses.com/easily-create-github-prs-from-magit.html
+     (defun endless/visit-pull-request-url ()
+       "Visit the current branch's PR on Github."
+       (interactive)
+       (browse-url
+        (format "https://github.com/%s/compare/%s"
+                (replace-regexp-in-string
+                 "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+                 (magit-get "remote"
+                            (magit-get-current-remote)
+                            "url"))
+                (magit-get-current-branch))))
+
+     (define-key magit-mode-map "V"
+       'endless/visit-pull-request-url)
+
+     (defadvice magit-blame-mode (after magit-blame-change-to-emacs-state activate compile)
+       "when entering magit blame mode, change evil normal state to emacs state"
+       (if (evil-normal-state-p)
+           (evil-emacs-state)
+         (evil-normal-state))
+       )
+
+     (ad-activate 'magit-blame-mode)
+
+     (defadvice git-timemachine-mode (after git-timemachine-change-to-emacs-state activate compile)
+       "when entering git-timemachine mode, change evil normal state to emacs state"
+       (if (evil-normal-state-p)
+           (evil-emacs-state)
+         (evil-normal-state)))
+
+     (ad-activate 'git-timemachine-mode)
+
+     (define-key magit-log-mode-map (kbd "W") 'magit-copy-item-as-kill)
+     ))
   )
+
+(defun my-misc/post-init-git-messenger ()
+  (use-package git-messenger
+    :defer t
+    :config
+    (progn
+     (defun my-vc-visit-file-revision (file rev)
+       "Visit revision REV of FILE in another window.
+With prefix argument, uses the current window instead.
+If the current file is named `F', the revision is named `F.~REV~'.
+If `F.~REV~' already exists, use it instead of checking it out again."
+       ;; based on `vc-revision-other-window'.
+       (interactive
+        (let ((file (expand-file-name
+                     (read-file-name
+                      (if (buffer-file-name)
+                          (format "File (%s): " (file-name-nondirectory
+                                                 (buffer-file-name)))
+                        "File: ")))))
+          (require 'vc)
+          (unless (vc-backend file)
+            (error "File %s is not under version control" file))
+          (list file (vc-read-revision
+                      "Revision to visit (default is working revision): "
+                      (list file)))))
+       (require 'vc)
+       (unless (vc-backend file)
+         (error "File %s is not under version control" file))
+       (let ((revision (if (string-equal rev "")
+                           (vc-working-revision file)
+                         rev))
+             (visit (if current-prefix-arg
+                        'switch-to-buffer
+                      'switch-to-buffer-other-window)))
+         (funcall visit (vc-find-revision file revision))))
+
+     (define-key git-messenger-map (kbd "f") 'my-vc-visit-file-revision)
+     )))
