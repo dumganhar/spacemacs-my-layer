24f6f8e7e4690b21625f040e6003dda2eb57a00f
andyque <guanghui8827@126.com>
2015-04-30 11:39:33 +0800

    add some better configs from prelude

--- my-better-defaults/config.el
+++ my-better-defaults/config.el
@@ -32,6 +32,8 @@
 (setq-default fill-column 80)
 
 (setq recenter-positions '(top middle bottom))
+;; delete the selection with a keypress
+(delete-selection-mode t)
 
 
 ;;add auto format paste code
@@ -63,3 +65,32 @@
       '("" " Guanghui - " (:eval (if (buffer-file-name)
                                                     (abbreviate-file-name (buffer-file-name))
                                    "%b"))))
+
+;; tramp, for sudo access
+(require 'tramp)
+;; keep in mind known issues with zsh - see emacs wiki
+(setq tramp-default-method "ssh")
+
+(set-default 'imenu-auto-rescan t)
+
+;; always delete and copy recursively
+(setq dired-recursive-deletes 'always)
+(setq dired-recursive-copies 'always)
+
+;; if there is a dired buffer displayed in the next window, use its
+;; current subdir, instead of the current subdir of this dired buffer
+(setq dired-dwim-target t)
+
+(defadvice ido-find-file (after find-file-sudo activate)
+  "Find file as root if necessary."
+  (unless (and buffer-file-name
+               (file-writable-p buffer-file-name))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+;; http://emacsredux.com/blog/2013/05/31/highlight-lines-that-exceed-a-certain-length-limit/
+(require 'whitespace)
+(setq whitespace-line-column 80) ;; limit line length
+(setq whitespace-style '(face lines-tail))
+
+;; (add-hook 'prog-mode-hook 'whitespace-mode)
+(global-whitespace-mode +1)
