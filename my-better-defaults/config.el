aefef23d5ecd7cb4530f886fbc42fcd86c3e5266
James Chen <jianhua.chen@cocos2d-x.org>
2015-06-11 21:16:14 +0800

    Updates user

--- my-better-defaults/config.el
+++ my-better-defaults/config.el
@@ -62,7 +62,7 @@
 ;; more useful frame title, that show either a file or a
 ;; buffer name (if the buffer isn't visiting a file)
 (setq frame-title-format
-      '("" " Guanghui - " (:eval (if (buffer-file-name)
+      '("" " James - " (:eval (if (buffer-file-name)
                                                     (abbreviate-file-name (buffer-file-name))
                                    "%b"))))
 
